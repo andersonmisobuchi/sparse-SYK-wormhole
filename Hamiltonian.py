e041ad6213c6301ab00cb3bf052a6b351163e78d
Anderson Misobuchi <andersm3@ThinkPad-X1C-5th-W10DG-7296049d.localdomain>
2022-01-15 16:27:18 -0600

    Added transmission

--- Hamiltonian.py
+++ Hamiltonian.py
@@ -5,6 +5,9 @@ from itertools import combinations
 from dynamite.extras import majorana
 from dynamite.operators import op_sum, op_product
 
+from dynamite.computations import eigsolve
+
+
 def hamiltonian_kitaev(N, q, random_seed):
     
     # Set seed to generate random couplings Jijkl
@@ -63,6 +66,26 @@ def hamiltonian_int(N, k, q, mu):
     majs = [majorana(i) for i in range(2*N)]
     HI = op_sum((op_product([majs[i], majs[i+N]]) for i in range(N)), nshow=N).scale(1j*mu/2)
     return HI
+
+# Define coupled SYK Hamiltonian from random regular hypergraph
+def hamiltonian_coupled(N, k, q, hyperedges, mu, random_seed):
+    np.random.seed(random_seed)
+    p = k*N/binom(N, q)
+    couplings = np.sqrt( factorial(q-1) / (p * N**(q-1) * 2**q) )*np.random.randn(len(hyperedges))
+    factor = dict(zip(hyperedges, couplings))
+    majs = [majorana(i) for i in range(2*N)]
+    HL = op_sum(op_product(majs[i] for i in idxs).scale(factor[idxs]) for idxs in hyperedges)
+    HR = op_sum(op_product(majs[i+N] for i in idxs).scale(factor[idxs]) for idxs in hyperedges)
+    HI = op_sum(majs[i]*majs[i+N] for i in range(N)).scale(1j*mu/2)
+    
+    return op_sum([HL, HR, HI])
+
+def get_gs(H):
+    '''
+    Get ground state as a dynamite State object from a Hamiltonian H
+    '''
+    _, gs = eigsolve(H, getvecs=True, nev=1, tol=1e-5)
+    return gs[0]
 
 def print_tex(H):
     # Clean expression for Hamiltonian output
